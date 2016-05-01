677f233e33ebb36c56c36eb31f7a998a21861dab
smiile8888 <th.thanyathorn@gmail.com>
2016-05-01 11:23:29 +0700

    Connect to Database

---
+++ src/database/ConnectDB.java
@@ -0,0 +1,23 @@
+
+package database;
+
+import edu.sit.cs.db.CSDbDelegate;
+
+public class ConnectDB {
+    
+    protected static CSDbDelegate db;
+    
+    public ConnectDB() {
+
+    }
+    
+    public static void connect() {
+        db = new CSDbDelegate("csprog-in.sit.kmutt.ac.th", "3306", "CSC105_G1", "CSC105_G1", "CSC105_G1");
+        System.out.println(db.connect());
+    }
+    
+    public static void disconnect() {
+        System.out.println(db.disconnect());
+    }
+
+}
